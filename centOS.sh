0463b9be93e393615d0658fccda4e7e3c910e396
Ryuseless <janfilipzacek@seznam.cz>
2023-04-01 16:56:07 +0200

    more syntax errors fixed i hope

--- centOS.sh
+++ centOS.sh
@@ -2,8 +2,8 @@
 systemctl disable rsyslog
 dnf -y install xorg-x11-server-Xorg xinit xterm
 touch ~/.xinitrc
-echo -e "#xinit scriptik\nxrandr -s 1024x768_60.00 &\nxterm -e \" echo 'xterm works'; sleep1; exit\" " >> ~/.xinitrc
+echo -e "#xinit scriptik\nxrandr -s 1024x768_60.00 \nxterm -e \" echo 'xterm works'; sleep1; exit\" " >> ~/.xinitrc
 startx || exit 1
-sed -i '3s/.*/xterm/' ~/.xinitrc.txt
+sed -i '3s/.*/xterm/' ~/.xinitrc
 #konec nahazovani xorg serveru
 echo "vic v budoucnu"
