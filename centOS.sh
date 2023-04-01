cf2f85ccab05dcfd4dd02b500cbaa197a464b33b
Ryuseless <janfilipzacek@seznam.cz>
2023-04-01 16:45:21 +0200

    wrong directory

--- centOS.sh
+++ centOS.sh
@@ -4,6 +4,6 @@ dnf -y install xorg-x11-server-Xorg xinit xterm
 touch ~/.xinitrc
 echo -e "#xinit scriptik\nxrandr -s 1024x768_60.00 &\nxterm -e \" echo 'xterm works'; sleep1; exit\" " >> ~/.xinitrc
 startx || exit 1
-sed -i '3s/.*/xterm/' file.txt
+sed -i '3s/.*/xterm/' ~/.xinitrc.txt
 #konec nahazovani xorg serveru
 echo "vic v budoucnu"
