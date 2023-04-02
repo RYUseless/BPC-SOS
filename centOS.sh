8bfb33ceb1c4a476afa19f7e290a866a303f8017
Ryuseless <janfilipzacek@seznam.cz>
2023-04-02 13:48:43 +0200

    browser + pdf reader?

--- centOS.sh
+++ centOS.sh
@@ -6,4 +6,11 @@ echo -e "#xinit scriptik\nxrandr -s 1024x768_60.00 &\n xterm -e \" echo 'xterm w
 startx || exit 1
 sed -i '3s/.*/xterm/' ~/.xinitrc
 #konec nahazovani xorg serveru
+wget https://copr.fedorainfracloud.org/coprs/bgstack15/palemoon/repo/epel-7/bg
+stack15-palemoon-epel-7.repo -O /etc/yum.repos.d/bgstack15-palemoon.repo
+--2019-03-01 21:39:26--  https://copr.fedorainfracloud.org/coprs/bgstack15/palem
+oon/repo/epel-7/bgstack15-palemoon-epel-7.repo
+dnf -y install palemoon
+dnf -y install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/g/gv-3.7.4-25.el8.x86_64.rpm
+startx || palemoon
 echo "vic v budoucnu"
