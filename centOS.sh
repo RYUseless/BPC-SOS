9c4fe01f0c59757887516561427da1dca7ca7a82
Ryuseless <janfilipzacek@seznam.cz>
2023-04-02 14:01:58 +0200

    i am too stupid

--- centOS.sh
+++ centOS.sh
@@ -9,7 +9,7 @@ sed -i '3s/.*/xterm/' ~/.xinitrc
 
 #browser + pdf reader
 dnf -y install wget
-wget https://copr.fedorainfracloud.org/coprs/bgstack15/palemoon/repo/epel-7/bg stack15-palemoon-epel-7.repo -O /etc/yum.repos.d/bgstack15-palemoon.repo --2019-03-01 21:39:26--  https://copr.fedorainfracloud.org/coprs/bgstack15/palem oon/repo/epel-7/bgstack15-palemoon-epel-7.repo
+wget https://copr.fedorainfracloud.org/coprs/bgstack15/palemoon/repo/epel-7/bg stack15-palemoon-epel-7.repo -O /etc/yum.repos.d/bgstack15-palemoon.repo
 yum -y install palemoon
 dnf -y install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/g/gv-3.7.4-25.el8.x86_64.rpm
 startx || palemoon
