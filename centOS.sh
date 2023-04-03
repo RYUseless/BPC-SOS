b770fede617e92b99b71d248f50f7cb8f66548ff
Ryuseless <janfilipzacek@seznam.cz>
2023-04-03 22:36:10 +0200

    added few lines to .bashrc section

--- centOS.sh
+++ centOS.sh
@@ -8,7 +8,7 @@ echo -ne '\n'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 printf "${RED}Crytical error, even universe couldnt make this work :(${NC}"
-sleep 8; clear
+sleep 5; clear
 # this is just me fooling around :)
 
 systemctl disable rsyslog 
@@ -27,7 +27,21 @@ yum -y install palemoon
 dnf -y install https://download-ib01.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/g/gv-3.7.4-25.el8.x86_64.rpm
 
 # .bashrc edit
-sed -i '1s/^/# placeholder for rm -rfv hellfest\n/' ~/.bashrc
+sed -i '1s/^/# - - E N D  O F   U T I S M - - \n/' ~/.bashrc
+#alias home='cd ~'
+#alias velikost='cd ~ && ./velikost.sh'
+# A L I A S for better life (when showing final result)
+#echo "pro zobrazeni velikosti, napiste 'velikost'" 
+clear
+sed -i '1s/^/# rm -rfv ~/.cache/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /var/log/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /var/cache/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /tmp/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /usr/tmp/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /var/tmp/*\n/' ~/.bashrc
+sed -i '1s/^/# rm -rfv /root/cache/*\n/' ~/.bashrc
+sed -i '1s/^/# rm  ~/.bash_history/*\n/' ~/.bashrc
+sed -i '1s/^/# rm  ~/.thumbnails/*\n/' ~/.bashrc
 sed -i '1s/^/XTRM=`pgrep xterm`\nif [ -z "$XTRM" ]; then\n	startx\nelse\n	resize -s 55 165\n fi\n/' ~/.bashrc
 sed -i '1s/^/# - - R Y U A  U T I S M - - \n/' ~/.bashrc
 
